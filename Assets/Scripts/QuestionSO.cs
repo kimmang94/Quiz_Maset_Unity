3cc72570655e96775e1b40e7df08f89ea99ed74f
kimhyeok <kimmang940715@naver.com>
2023-09-25 18:36:54 +0900

    스크립터블 정답 추가

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,24 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     private string questuion = "Enter new question text here";
 
+    [SerializeField]
+    private string[] answers = new string[4];
+
+    [SerializeField]
+    private int correctAnswerIndex;
     public string GetQuestion()
     {
         return questuion;
     }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
     
-    
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
     
 } 
