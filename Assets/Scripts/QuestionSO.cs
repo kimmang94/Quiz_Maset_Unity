374680c563a3236fffb802f8d183f4fc3ecd616f
kimhyeok <kimmang940715@naver.com>
2023-09-25 10:06:16 +0900

    스크립터블 오브젝트 생성

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Questin", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [SerializeField]
+    [TextArea(2, 6)]
+    private string questuion = "Enter new question text here";
+    
+    
+}
