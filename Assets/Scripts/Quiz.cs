93e12ff2b317f0063c42b7e9fa4728abf72718f4
kimhyeok <kimmang940715@naver.com>
2023-09-26 22:35:28 +0900

    정답 누르는 버튼기능 및 스프라이트 스왑 기능 추가

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 
 
 public class Quiz : MonoBehaviour
@@ -13,6 +14,12 @@ public class Quiz : MonoBehaviour
     private QuestionSO question = null;
     [SerializeField]
     private GameObject[] answerButtons;
+
+    private int correctAnswerIndex = 0;
+    [SerializeField]
+    private Sprite defaultSprite= null;
+    [SerializeField]
+    private Sprite correctAnswerSprite = null;
     
     private void Start()
     {
@@ -22,6 +29,29 @@ public class Quiz : MonoBehaviour
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    /// <summary>
+    /// 정답 index를 선택해서 정답이면 정답효과, 오답시 오답효과
+    /// </summary>
+    /// <param name="index"></param>
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was; \n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 
